d0ab5dd90554373fc214cf297127eeb67e6a4b3c
BogPomozet <vkerechaninaa@gimail.com>
2022-11-26 00:41:05 +0000

    home page made

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from flask import Flask, render_template, request, flash
 
-from website import init
+app = Flask(__name__)
+# encription to sequre cookies and session data
+# app.config['SECRET_KEY'] = 'petri_secret_@@'
 
-app = init()
+
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
