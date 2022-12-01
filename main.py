30a18e23dc420973c42a8a28b3757a23f1c020f3
BogPomozet <vkerechaninaa@gimail.com>
2022-12-01 16:44:02 +0000

    Added introductory example

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def checklogin():
     UN = request.form['Username']
     PW = request.form['Password']
 
-    sqlconnection = sqlite3.Connection(currentlocation + "\Login.db")
+    sqlconnection = sqlite3.Connection(currentlocation + "\Login")
     cursor = sqlconnection.cursor()
     query1 = "SELECT Username, Password From Users WHERE Username = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
 
@@ -43,6 +43,7 @@ def registerpage():
         sqlconnection.commit()
         return redirect("/")
     return render_template("register.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
