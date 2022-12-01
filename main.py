7ec9337ab2373e2233e27cba97fd3beaa663b873
BogPomozet <vkerechaninaa@gimail.com>
2022-12-01 01:21:24 +0000

    register form in progress

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, request, session, url_for
+import os
+import sqlite3
+
+currentlocation = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 # encription to sequre cookies and session data
@@ -9,6 +13,36 @@ app = Flask(__name__)
 def index(): 
     return render_template("index.html")
 
+@app.route("/", methods = ["POST"])
+def checklogin():
+    UN = request.form['Username']
+    PW = request.form['Password']
+
+    sqlconnection = sqlite3.Connection(currentlocation + "\Login.db")
+    cursor = sqlconnection.cursor()
+    query1 = "SELECT Username, Password From Users WHERE Username = '{un}' AND Password = '{pw}'".format(un = UN, pw = PW)
+
+    rows = cursor.execute(query1)
+    rows = rows.fetchall()
+    if len(rows) == 1:
+        return render_template("loggedin.html")
+    else:
+        return redirect("/register")
+
+@app.route("/register", methods = ["GET","POST"])
+def registerpage():
+    if request.method == "POST":
+        dUN = request.form['DUsername']
+        dPW = request.form['DPassword']
+        Uemail = request.form['EmailUser']
+
+        sqlconnection = sqlite3.Connection(currentlocation + "\Login")
+        cursor = sqlconnection.cursor()
+        query1 = "INSERT INTO Users VALUES('{u}','{p}','{e}')".format( u=dUN, p=dPW, e=Uemail)
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect("/")
+    return render_template("register.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
